f5f49f6a8756ebbb0d926f9bd9b4d63a2d0dd60b
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-07-11 07:57:48 +0700

    install graphql package

--- src/app/arit/page.jsx
+++ src/app/arit/page.jsx
@@ -1,9 +1,31 @@
+import { request, gql } from 'graphql-request';
 
+export async function fetchData() {
+  const endpoint = 'https://pr.web.rmutsv.ac.th/graphql'; // Replace with your WordPress GraphQL endpoint
 
-const page = () => {
-  return (
-    <div>Welcome to Arit</div>
-  )
+  const query = gql`
+    query {
+      posts {
+        edges {
+          node {
+            slug
+            featuredImage {
+              node {
+                altText
+                sourceUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const data = await request(endpoint, query);
+    return data.posts.edges;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
 }
-
-export default page
