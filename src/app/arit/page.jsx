4d4f0aef3939cb874efca9659162eb55dd8af72b
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-07-11 13:46:55 +0700

    edit carousel

--- src/app/arit/page.jsx
+++ src/app/arit/page.jsx
@@ -1,13 +1,31 @@
 "use client"
 import { useEffect, useState } from 'react';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-
-export default function Posts() {
+export default function Posts(props) {
     const [posts, setPosts] = useState([]);
-
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+    
     useEffect(() => {
         const fetchPosts = async () => {
             const client = new ApolloClient({
@@ -55,12 +73,22 @@ export default function Posts() {
             </nav>
 
             <Carousel
-                    showThumbs={false}
-                    showStatus={false}
-                    autoPlay={true}
-                    interval={5000}
-                    infiniteLoop={true}
-                    dynamicHeight={true}
+                   swipeable={false}
+                   draggable={false}
+                   showDots={true}
+                   responsive={responsive}
+                   ssr={true} // means to render carousel on server-side.
+                   infinite={true}
+                   autoPlay={props.deviceType !== "mobile" ? true : false}
+                   autoPlaySpeed={5000}
+                   keyBoardControl={true}
+                   customTransition="ease-in all .5"
+                   transitionDuration={100}
+                   containerClass="carousel-container"
+                   removeArrowOnDeviceType={["tablet", "mobile"]}
+                   deviceType={props.deviceType}
+                   dotListClass="custom-dot-list-style"
+                   itemClass="carousel-item-padding-40-px"
                
                 >
                     {posts.map((post) => (
@@ -70,6 +98,7 @@ export default function Posts() {
                                 <img
                                     src={post.node.featuredImage.node.sourceUrl}
                                     alt={post.node.featuredImage.node.altText}
+                                    className='h-auto max-w-full'
                                 />
                             )}
                         </div>
