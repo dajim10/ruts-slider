ad055aa15731a6bd942487b589fa77e70dd3bb69
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-07-11 08:12:38 +0700

    success point

--- src/app/arit/page.jsx
+++ src/app/arit/page.jsx
@@ -1,31 +1,64 @@
-import { request, gql } from 'graphql-request';
+"use client"
+import { useEffect, useState } from 'react';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export async function fetchData() {
-  const endpoint = 'https://pr.web.rmutsv.ac.th/graphql'; // Replace with your WordPress GraphQL endpoint
+export default function Posts() {
+    const [posts, setPosts] = useState([]);
 
-  const query = gql`
-    query {
-      posts {
-        edges {
-          node {
-            slug
-            featuredImage {
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const client = new ApolloClient({
+                uri: 'https://pr.web.rmutsv.ac.th/graphql', // Replace with your WordPress GraphQL endpoint
+                cache: new InMemoryCache(),
+            });
+
+            const query = gql`
+        query {
+          posts {
+            edges {
               node {
-                altText
-                sourceUrl
+                slug
+                featuredImage {
+                  node {
+                    altText
+                    sourceUrl
+                  }
+                }
               }
             }
           }
         }
-      }
-    }
-  `;
+      `;
 
-  try {
-    const data = await request(endpoint, query);
-    return data.posts.edges;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
+            try {
+                const { data } = await client.query({ query });
+                console.log(data)
+                setPosts(data.posts.edges);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    return (
+        <div>
+            {/* Render your posts data */}
+            {posts.map((post) => (
+                <div key={post.node.slug}>
+                    <h2>{post.node.slug}</h2>
+                    {post.node.featuredImage && post.node.featuredImage.node && (
+            <img
+              src={post.node.featuredImage.node.sourceUrl}
+              alt={post.node.featuredImage.node.altText}
+            />
+          )}
+                    {/* <p>{post.node.featuredImage}</p> */}
+                    {/* <img src={post.node.featuredImage.node.sourceUrl} alt={post.node.featuredImage.node.altText} /> */}
+                </div>
+            ))}
+        </div>
+    );
 }
