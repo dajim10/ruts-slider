e91d6dd1178704988799252a75134062e64df58d
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-07-16 15:31:43 +0700

    edit first page

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "rmutsv.ac.th",
                 port: "",
-                pathname:"/**",
+                pathname: "/**",
             }
         ]
     }
