84b59cb4ea51922f11f9403b67e95631d4f162ad
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-07-18 00:12:18 +0700

    edit next config

---
+++ _static/_next/static/chunks/app/page-43824359ce6628e4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8853:function(n,e,u){Promise.resolve().then(u.t.bind(u,7095,23))}},function(n){n.O(0,[95,253,698,744],function(){return n(n.s=8853)}),_N_E=n.O()}]);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    reactStrictMode: true,
+    output: 'export',
+    distDir: '_static',
+    images: {
+        unoptimized:true
+    }
+
 }
 
 module.exports = nextConfig
